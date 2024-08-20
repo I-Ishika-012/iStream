40e608865c73221804fdd9a58b5e47d40668592e
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-08-20 20:50:52 +0530

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,43 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import GoogleIcon from '@mui/icons-material/Google';
+
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon /> },
+  { name: 'Google', icon: <GoogleIcon /> },
+  { name: 'Coding', icon: <CodeIcon /> },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon /> },
+  { name: 'Podcast', icon: <GraphicEqIcon /> },
+  { name: 'Movie', icon: <OndemandVideoIcon /> },
+  { name: 'Gaming', icon: <SportsEsportsIcon /> },
+  { name: 'Live', icon: <LiveTvIcon /> },
+  { name: 'Sport', icon: <FitnessCenterIcon /> },
+  { name: 'Fashion', icon: <CheckroomIcon /> },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon /> },
+  { name: 'Comedy', icon: <TheaterComedyIcon /> },
+  { name: 'Workout', icon: <FitnessCenterIcon /> },
+  { name: 'News', icon: <LiveTvIcon /> },
+  { name: 'Crypto', icon: <DeveloperModeIcon /> }
+];
+
+export const demoThumbnailUrl = 'https://img.freepik.com/free-vector/halftone-background-with-circles_23-2148907689.jpg?t=st=1724165826~exp=1724169426~hmac=564beb8acd232c52cfc60bc693e6b4f00c149e5e004491ac782105594717b15f&w=740';
+export const demoChannelUrl = '/@GoogleDevelopers';
+export const demoVideoUrl = '/video/Z8F6FvMrN4o';
+export const demoChannelTitle = 'Google Developers';
+export const demoVideoTitle = 'Build and Deploy 5 API Projects in 10 Hours';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
